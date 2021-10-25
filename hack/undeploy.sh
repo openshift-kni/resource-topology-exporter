d69cba92aa4ae2cf522f6f9cf7175978aa28103b
Talor Itzhak <titzhak@redhat.com>
2021-10-25 19:17:13 +0300

    Add deploy/undeploy RTE scripts
    The scripts will deploy/undeploy RTE with all its components on a separate namespace.
    This is very useful in order to deploy RTE prior to e2e-testing or for users to experience with it.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ hack/undeploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# expect oc to be in PATH by default
+OC_TOOL="${OC_TOOL:-oc}"
+RTE_NAMESPACE="${RTE_NAMESPACE:-rte-e2e}"
+
+echo "Undeploy RTE"
+"$OC_TOOL" delete clusterrolebinding handle-rte
+"$OC_TOOL" delete clusterrole rte-handler
+"$OC_TOOL" delete ns "${RTE_NAMESPACE}"
