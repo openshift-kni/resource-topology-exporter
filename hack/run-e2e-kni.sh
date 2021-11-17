bbb38101d2bd52e66dc1eb3deab97ead34666ab2
Talor Itzhak <titzhak@redhat.com>
2021-11-17 15:42:18 +0200

    Export env variable
    Export all the variable so child processes will have them as well
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- hack/run-e2e-kni.sh
+++ hack/run-e2e-kni.sh
@@ -8,13 +8,13 @@ OC_TOOL="${OC_TOOL:-oc}"
 BASE_DIR="$(dirname "$(realpath "$0")")"
 PROJECT_DIR="${BASE_DIR}"/..
 
-RTE_NAMESPACE="${RTE_NAMESPACE:-rte-e2e}"
-E2E_NAMESPACE_NAME="${E2E_NAMESPACE_NAME:-rte-e2e}"
-E2E_TOPOLOGY_MANAGER_POLICY="${E2E_TOPOLOGY_MANAGER_POLICY:-single-numa-node}"
+export RTE_NAMESPACE="${RTE_NAMESPACE:-rte-e2e}"
+export E2E_NAMESPACE_NAME="${E2E_NAMESPACE_NAME:-rte-e2e}"
+export E2E_TOPOLOGY_MANAGER_POLICY="${E2E_TOPOLOGY_MANAGER_POLICY:-single-numa-node}"
 
 make -C "${PROJECT_DIR}" kube-update
 make -C "${PROJECT_DIR}" wait-for-mcp
 echo "Kubelet configured properly"
 
-RTE_NAMESPACE="${RTE_NAMESPACE:-rte-e2e}" "${BASE_DIR}"/setup-e2e.sh
+"${BASE_DIR}"/setup-e2e.sh
 make -C "${PROJECT_DIR}" test-e2e
